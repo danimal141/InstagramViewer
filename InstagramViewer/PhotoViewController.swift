4d9af2992e2efdf6f47459e7a850fbdb76fa0df0
danimal141 <hideaki.ishii1204@gmail.com>
2015-03-15 17:00:37 +0900

    Add PhotoViewController and adjust to show the standard image

---
+++ InstagramViewer/PhotoViewController.swift
@@ -0,0 +1,28 @@
+//
+//  PhotoViewController.swift
+//  InstagramViewer
+//
+//  Created by Hideaki Ishii on 3/15/15.
+//  Copyright (c) 2015 danimal141. All rights reserved.
+//
+
+import UIKit
+
+class PhotoViewController: UIViewController {
+
+    @IBOutlet weak var standardImage: UIImageView!
+    
+    var photo: Photo?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        let data = NSData(contentsOfURL: self.photo!.standardURL!)
+        self.standardImage!.image = UIImage(data: data!)
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
